affcdb554e624a098d1d2ff74874b95e3e32f11c
Mitushi <mitushi1428@gmail.com>
2025-08-31 19:16:20 +0530

    RED: add test for empty string returns 0

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add('')).to eq(0)
+    end
+  end
+end
